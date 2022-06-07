1fb40679f89770b5095e7b2403b50096963dd4d4
Cristal <camacri2@alumni.uv.es>
2022-06-07 11:57:11 +0200

    feat(front): add 'new category' dialog

--- client/src/app/category/category-list/category-list.component.ts
+++ client/src/app/category/category-list/category-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { Category } from '../model/Category';
 import { CategoryService } from '../category.service';
+import { MatDialog } from '@angular/material/dialog';
+import { CategoryEditComponent } from '../category-edit/category-edit.component';
 
 @Component({
   selector: 'app-category-list',
@@ -15,12 +17,23 @@ export class CategoryListComponent implements OnInit {
 
   constructor(
     private categoryService: CategoryService,
+    public dialog: MatDialog,
   ) { }
 
   ngOnInit(): void {
     this.categoryService.getCategories().subscribe(
       categories => this.dataSource.data = categories
     );
+  }
+
+  createCategory() {
+    const dialogRef = this.dialog.open(CategoryEditComponent, {
+      data: {}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      this.ngOnInit();
+    });
   }
 
 }
--- client/src/app/category/category.module.ts
+++ client/src/app/category/category.module.ts
@@ -4,14 +4,23 @@ import { MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { CategoryListComponent } from './category-list/category-list.component';
+import { CategoryEditComponent } from './category-edit/category-edit.component';
+import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
-  declarations: [CategoryListComponent],
+  declarations: [CategoryListComponent, CategoryEditComponent],
   imports: [
     CommonModule,
     MatTableModule,
     MatIconModule, 
-    MatButtonModule
+    MatButtonModule,
+    MatDialogModule,
   ],
+  providers: [
+    {
+      provide: MAT_DIALOG_DATA,
+      useValue: {},
+    },
+  ]
 })
 export class CategoryModule { }
