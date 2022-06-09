a9d8115a7f618b53be99dd7abe760ae7a08d56f5
Cristal <camacri2@alumni.uv.es>
2022-06-09 11:51:09 +0200

    feat(front): add save and delete, DB access

--- client/src/app/category/category.module.ts
+++ client/src/app/category/category.module.ts
@@ -1,4 +1,4 @@
-// IMPORTS
+// IMPORTS 
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatTableModule } from '@angular/material/table';
@@ -10,6 +10,7 @@ import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 // DEPENDENCIES
 @NgModule({
@@ -24,6 +25,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     MatInputModule,
     FormsModule,
     ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [
     {
--- client/src/app/category/category.service.ts
+++ client/src/app/category/category.service.ts
@@ -1,29 +1,35 @@
 // IMPORTS
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Category } from './model/Category';
-import { CATEGORY_DATA } from './model/mock-categories';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 // CLASS DEFINITION
 export class CategoryService {
 
-  constructor() { }
+    constructor(
+        private http: HttpClient
+    ) { }
 
-  // Getter
-  getCategories(): Observable<Category[]> {
-    return of(CATEGORY_DATA);
-  }
+    // Getter
+    getCategories(): Observable<Category[]> {
+        return this.http.get<Category[]>('http://localhost:8080/category');
+    }
 
-  // Setter
-  saveCategory(category: Category): Observable<Category> {
-    return of(null);
-  }
+    // Setter
+    saveCategory(category: Category): Observable<Category> {
+        let url = 'http://localhost:8080/category';
+        if (category.id != null)
+            url += '/' + category.id;
+        
+        return this.http.put<Category>(url, category);
+    }
 
-  // Delete
-  deleteCategory(idCategory: number): Observable<any> {
-    return of(null);
-  }
+    // Delete
+    deleteCategory(idCategory: number): Observable<any> {
+        return this.http.delete('http://localhost:8080/category/' + idCategory);
+    }
 }
