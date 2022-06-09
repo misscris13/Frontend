908a7745736c5c2e83c4a82a1b00fc61d70832e9
Cristal <camacri2@alumni.uv.es>
2022-06-09 13:43:52 +0200

    feat(front): add edit dialog

--- client/src/app/author/author-edit/author-edit.component.ts
+++ client/src/app/author/author-edit/author-edit.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+// IMPORTS
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { AuthorService } from '../author.service';
+import { Author } from '../model/Author';
 
+// COMPONENT
 @Component({
-  selector: 'app-author-edit',
-  templateUrl: './author-edit.component.html',
-  styleUrls: ['./author-edit.component.scss']
+    selector: 'app-author-edit',
+    templateUrl: './author-edit.component.html',
+    styleUrls: ['./author-edit.component.scss']
 })
+// CLASS DEFINITION
 export class AuthorEditComponent implements OnInit {
+    author : Author;
 
-  constructor() { }
+    constructor(
+        public dialogRef: MatDialogRef<AuthorEditComponent>,
+        @Inject(MAT_DIALOG_DATA) public data: any,
+        private authorService: AuthorService,
+    ) { }
 
-  ngOnInit(): void {
-  }
+    // On initialization, create or edit an author
+    ngOnInit(): void {
+        if (this.data.author != null) {
+            this.author = Object.assign({}, this.data.author);
+        }
+        else {
+            this.author = new Author();
+        }
+    }
 
+    // Create or edit
+    onSave() {
+        this.authorService.saveAuthor(this.author).subscribe(result => {
+            this.dialogRef.close();
+        });
+    }
+
+    // Close dialog
+    onClose() {
+        this.dialogRef.close();
+    }
 }
