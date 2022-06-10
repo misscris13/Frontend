98c309836cf91c688c5170f88b5e0d49d9f95ee8
Cristal <camacri2@alumni.uv.es>
2022-06-10 12:12:54 +0200

    feat(project): connect back with front

--- client/src/app/author/author.service.ts
+++ client/src/app/author/author.service.ts
@@ -1,4 +1,5 @@
 // IMPORTS
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Pageable } from '../core/model/page/Pageable';
@@ -12,17 +13,24 @@ import { AUTHOR_DATA } from './model/mock-authors';
 // CLASS DEFINITION
 export class AuthorService {
 
-    constructor() { }
+    constructor(
+        private http: HttpClient
+    ) { }
 
     getAuthors(pageable: Pageable): Observable<AuthorPage> {
-        return of(AUTHOR_DATA);
+        return this.http.post<AuthorPage>('http://localhost:8080/author', {pageable: pageable});
     }
 
     saveAuthor(author: Author): Observable<void> {
-        return of(null);
+        let url = 'http://localhost:8080/author';
+        
+        if (author.id != null)
+            url += '/' + author.id;
+        
+        return this.http.put<void>(url, author);
     }
 
     deleteAuthor(idAuthor: number): Observable<void> {
-        return of(null);
+        return this.http.delete<void>('http://localhols:8080/author/' + idAuthor);
     }
 }
