5f9e5ebef86a023d1620c8c4bd020b78deaecb8e
Cristal <camacri2@alumni.uv.es>
2022-06-14 14:38:09 +0200

    feat(project): connect front and back

--- client/src/app/client/client.module.ts
+++ client/src/app/client/client.module.ts
@@ -8,8 +8,8 @@ import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 import { ClientEditComponent } from './client-edit/client-edit.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
     declarations: [
--- client/src/app/client/client.service.ts
+++ client/src/app/client/client.service.ts
@@ -2,16 +2,19 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Client } from './model/Client';
 import { CLIENT_DATA } from './model/mock-clients'
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
     providedIn: 'root'
 })
 export class ClientService {
 
-    constructor() { }
+    constructor(
+        private http: HttpClient,
+    ) { }
 
     getClients(): Observable<Client[]> { 
-        return of(CLIENT_DATA);
+        return this.http.get<Client[]>('http://localhost:8080/client');
     }
 
     saveClient(client: Client): Observable<Client> {
