0566fc64c6cae5aeb0d623923b94bdbff2b47452
Cristal <camacri2@alumni.uv.es>
2022-06-16 09:47:40 +0200

    feat(front): add edit and delete clients

--- client/src/app/client/client.service.ts
+++ client/src/app/client/client.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Client } from './model/Client';
-import { CLIENT_DATA } from './model/mock-clients'
+// import { CLIENT_DATA } from './model/mock-clients'
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
@@ -18,10 +18,15 @@ export class ClientService {
     }
 
     saveClient(client: Client): Observable<Client> {
-        return of(null);
+        let url = "http://localhost:8080/client"
+
+        if (client.id != null)
+            url += "/" + client.id;
+
+        return this.http.put<Client>(url, client);
     }
 
     deleteClient(idClient: number): Observable<any> {
-        return of(null);
+        return this.http.delete('http://localhost:8080/category');
     }
 }
