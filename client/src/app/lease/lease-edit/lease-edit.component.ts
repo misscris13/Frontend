08cdddfee1be3d8c7a71daa1e997fd5f464d028e
Cristal <camacri2@alumni.uv.es>
2022-06-23 12:38:03 +0200

    fix(front): show correct data when editing lease

--- client/src/app/lease/lease-edit/lease-edit.component.ts
+++ client/src/app/lease/lease-edit/lease-edit.component.ts
@@ -24,6 +24,7 @@ export class LeaseEditComponent implements OnInit {
     games: Game[];
     clients: Client[];
 
+    errorDates: Boolean;
     tooManyLeases: Boolean;
     tooManyDays: Boolean;
     alreadyLent: Boolean;
@@ -50,43 +51,62 @@ export class LeaseEditComponent implements OnInit {
         // Get all games
         this.gameService.getGames().subscribe( games => {
             this.games = games;
+
+            if (this.lease.game != null) {
+                let gameFilter: Game[] = games.filter(game => game.id == this.data.lease.game.id);
+                if (gameFilter != null) {
+                    this.lease.game = gameFilter[0];
+                }
+            }
         });
 
         // Get all clients
         this.clientService.getClients().subscribe( clients => {
             this.clients = clients;
+
+            if (this.lease.client != null) {
+                let clientFilter: Client[] = clients.filter(client => client.id == this.data.lease.client.id);
+                if (clientFilter != null) {
+                    this.lease.client = clientFilter[0];
+                }
+            }
         });
 
         // Get all leases
-        this.leaseService.getAll().subscribe( leases => {
-            this.leases = leases;
-        })
+        this.leaseService.getAll().subscribe( 
+            leases => this.leases = leases
+        );
 
-        this.leases.forEach(l => this.lentGames.push(l.game));
-        this.leases.forEach(l => this.lentClients.push(l.client));
+        //this.leases.forEach(l => this.lentGames.push(l.game));
+        //this.leases.forEach(l => this.lentClients.push(l.client));
 
         this.error = false;
-        this.errorMessage = "";
     }
 
     // Saves the lease
     onSave() {
-        this.tooManyDays = (this.lease.endDate.valueOf() - this.lease.startDate.valueOf()) > 14;
-        if (this.tooManyDays)
-            this.errorMessage += "\n No se puede prestar por más de 14 días.";
+        // this.errorDates = (this.lease.startDate > this.lease.endDate);
+        // if (this.errorDates)
+        //     this.errorMessage = "La fecha de fin no puede ser menor que la de inicio. \n"
+        // else
+        //     this.tooManyDays = (Math.ceil((this.lease.endDate.valueOf() - this.lease.startDate.valueOf())/(1000*3600*24))) > 14;
 
-        this.tooManyLeases = this.lentClients.indexOf(this.lease.client) > -1;
-        if (this.tooManyLeases)
-            this.errorMessage += "\n Este cliente ya tiene un juego prestado.";
+        // if (this.tooManyDays)
+        //     this.errorMessage = "No se puede prestar por más de 14 días. \n";
+
+        //this.tooManyLeases = this.lentClients.indexOf(this.lease.client) > -1;
+        //if (this.tooManyLeases)
+        //    this.errorMessage += "Este cliente ya tiene un juego prestado. \n";
 
         //this.alreadyLent = this.lentGames.indexOf(this.lease.game) > -1;
         this.alreadyLent = false;
 
-        this.error = this.tooManyDays || this.tooManyLeases || this.alreadyLent;
+        this.error = this.errorDates || this.tooManyDays || this.tooManyLeases || this.alreadyLent;
 
-        this.leaseService.saveLease(this.lease).subscribe( result => {
-            this.dialogRef.close();
-        });
+        if (!this.error)
+            this.leaseService.saveLease(this.lease).subscribe( result => {
+                this.dialogRef.close();
+            });
     }
 
     // Closes the dialog
